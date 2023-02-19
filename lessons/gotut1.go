17606f3b29d928af2a8a91233ba19902b9687375
Marcin Maciorowski <marmac2525@gmail.com>
2023-02-19 23:37:43 +0100

    Introduction to the language

---
+++ lessons/gotut1.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	f "fmt"
+	"log"
+	"os"
+)
+
+var pl = f.Println
+
+func main() {
+	pl("Hello Go")
+	fmt.Print("What is your name? ")
+	reader := bufio.NewReader(os.Stdin)
+	name, err := reader.ReadString('\n')
+	if err == nil {
+		pl("Hello", name)
+	} else {
+		log.Fatal(err)
+	}
+}

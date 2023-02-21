879aa9c69ad90998c1572ff25e7e4327a5ad75f1
Marcin Maciorowski <marmac2525@gmail.com>
2023-02-21 19:37:57 +0100

    Working with ifAndFormating

---
+++ lessons/CoreGoLanguage/ifAndFormating.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+var pl = fmt.Println
+
+func main() {
+	// If conditional
+	// Conditional operators: > < >= <= == !=
+	// Logical Operators: && || !
+
+	iAge := 8
+	if (iAge >= 1) && (iAge <= 18) {
+		pl("Important Birthday")
+	} else if (iAge == 21) || (iAge == 18) {
+		pl("Important Birthday")
+	} else if iAge >= 65 {
+		pl("Important Birthday")
+	} else {
+		pl("Not an important birthday")
+	}
+
+	pl("!true =", !true)
+
+	// ----- FORMATTED PRINT -----
+	// Go has its own version of C's printf
+	// %d : Integer
+	// %c : Character
+	// %f : Float
+	// %t : Boolean
+	// %s : String
+	// %o : Base 8
+	// %x : Base 16
+	// %v : Guesses based on data type
+	// %T : Type of supplied value
+
+	fmt.Printf("%s %d %c %f %t %o %x\n", "Stuff", 1, 'A', 4.3, true, 1, 2)
+	fmt.Printf("%9f\n", 3.14)
+	fmt.Printf("%.2f\n", 3.141592)
+	fmt.Printf("%9.f\n", 3.141592)
+	sp1 := fmt.Sprintf("%9.f\n", 3.141592)
+	pl(sp1)
+}

e0324543c98aed7b24af6fd18de1cc67a95bb0be
Marcin Maciorowski <marmac2525@gmail.com>
2023-02-25 19:07:37 +0100

    Working with command line and compiled app

---
+++ lessons/CoreGoLanguage/commandLine.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// go build .\commandLine.go
+// .\commandLine.exe 11,2,11
+// Max value: 11
+
+func main() {
+	fmt.Println(os.Args)
+	args := os.Args[1:]
+	var iArgs = []int{}
+	for _, i := range args {
+		val, err := strconv.Atoi(i)
+		if err != nil {
+			panic(err)
+		}
+		iArgs = append(iArgs, val)
+	}
+	max := 0
+	for _, val := range iArgs {
+		if val > max {
+			max = val
+		}
+	}
+	fmt.Println("Max value: ", max)
+}

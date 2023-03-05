e34087786fa4e721598dbe41e288a32f8c36ae10
Marcin Maciorowski <marmac2525@gmail.com>
2023-03-05 19:50:58 +0100

    Working with sessions and csrf

--- web3/cmd/web/main.go
+++ web3/cmd/web/main.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	"net/http"
+	"time"
 	"web3/pkg/config"
 	"web3/pkg/handlers"
 	"web3/pkg/helpers"
+
+	"github.com/alexedwards/scs/v2"
 )
 
+var sessionManager *scs.SessionManager
+var app config.AppConfig
+
 func main() {
-	var app config.AppConfig
+	sessionManager = scs.New()
+	sessionManager.Lifetime = 24 * time.Hour
+	sessionManager.Cookie.Persist = true
+	// cookies secure is falce because in development there is no https connection
+	sessionManager.Cookie.Secure = false
+	sessionManager.Cookie.SameSite = http.SameSiteLaxMode
+	app.Session = sessionManager
 
 	repo := handlers.NewRepo(&app)
 	handlers.NewHandlers(repo)
